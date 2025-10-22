6dba677368009b74bce1e198de8f3f74e7c18a50
tungnx789 <tungnx789@gmail.com>
2025-10-22 23:26:52 +0700

    Update API URL with real Web App URL

--- js/admin.js
+++ js/admin.js
@@ -78,7 +78,7 @@ class QLBHAdmin {
     async callAPI(action, params = {}) {
         this.showLoading();
         try {
-            const url = new URL('https://script.google.com/macros/s/1QRblo37Vooehfn8um9lNuvcAUqYbqpeHH1zJR6PPX-J6qNj791YrIZZQ/exec');
+            const url = new URL('https://script.google.com/macros/s/AKfycbxxyHca1zVXW9fa5FSFUP7XEtkaxvTMojxE-O9Vqk1LyyBYE7Aig42sr4ZXfwCXms5bkA/exec');
             url.searchParams.append('action', action);
             Object.keys(params).forEach(key => {
                 url.searchParams.append(key, params[key]);
